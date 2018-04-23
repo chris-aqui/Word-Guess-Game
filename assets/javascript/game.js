2250b970aa7f24f225a415d6b21d5f20dde99a01
christine-aqui <2739361+christine-aqui@users.noreply.github.com>
2018-04-22 21:59:10 -0400

    added array with objects containing name, image and a hint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,37 @@
 console.log('connected');
+//
+var charactersList = [
+  {
+    "name": 'Buzz Lightyear',
+    "image":'../images/Buzz.jpg',
+    "hint": 'To Infinity and Beyond'
+  },
+  {
+    "name": 'Sheriff Woody',
+    "image":'../images/woody.png',
+    "hint": 'Reach for the sky!'
+  },
+  {
+    "name": 'Potato Head',
+    "image":'../images/mr-potato-head.gif',
+    "hint": 'Hey, Hamm! Look, I\'m Picasso!'
+  },
+  {
+    "name": 'Rex',
+    "image":'../images/Rex.png',
+    "hint": 'Any dinosaur-shaped ones?'
+  },
+  {
+    "name": 'Jessie',
+    "image":'../images/Jessie.png',
+    "hint": 'Buzz Lightyear\'s girlfriend'
+  },
 
+]
+//
 document.onkeyup = function (event){
   var userEnter = event.key.toLowerCase();
+
   if(userEnter === "enter"){
     console.log('User pressed enter. ' + userEnter);
   }else {
