1d850255f3a23aaca8941aa95e3bda2d741c7f96
christine-aqui <2739361+christine-aqui@users.noreply.github.com>
2018-04-22 21:15:17 -0400

    tested keys detection in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,16 @@
 console.log('connected');
 
+document.onkeyup = function (event){
+  var userEnter = event.key.toLowerCase();
+  if(userEnter === "enter"){
+    console.log('User pressed enter. ' + userEnter);
+  }else {
+    console.log('not enter');
+  }
+console.log(userEnter)
+}
+
+
+// var word = "<p>User pressed " + userEnter + "</p>";
+// next line writes to the screen
+// document.querySelector("#gameSpace").innerHTML = word;
