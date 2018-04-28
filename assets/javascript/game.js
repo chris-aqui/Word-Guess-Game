8f4109d26f31c3ab709b118ffaea6ce5cf378f88
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-04-28 14:29:12 -0400

    figured out why the test word was not working the way i expected... i had a forloop and i should of been using a math.random instead.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,14 @@
 console.log('connected');
-//
+// variable
 var charactersList = [{
-    "name": 'Buzz Lightyear',
+    "name": 'Buzz',
     "image": '../images/Buzz.jpg',
     "hint": 'To Infinity and Beyond'
   },
   {
-    "name": 'Sheriff Woody',
+    "name": 'Woody',
     "image": '../images/woody.png',
     "hint": 'Reach for the sky!'
-  },
-  {
-    "name": 'Potato Head',
-    "image": '../images/mr-potato-head.gif',
-    "hint": 'Hey, Hamm! Look, I\'m Picasso!'
   },
   {
     "name": 'Rex',
@@ -25,41 +20,73 @@ var charactersList = [{
     "image": '../images/Jessie.png',
     "hint": 'Buzz Lightyear\'s girlfriend'
   },
-
 ]
-//
-document.onkeyup = function (event) {
-  var userEnter = event.key.toLowerCase();
-  var userAnswer = document.querySelector('#answerF').value;
-  // console.log('User pressed enter. ' + userEnter);
-  // displays the hint
-  for (var i = 0; i < charactersList.length; i++) {
-    if (userEnter === "enter") {
-      console.log(userEnter);
-      var userHint = charactersList[i].hint;
-      var question = charactersList[i].name;
-      var questimg = charactersList[i].image;
-      //
-      document.querySelector("#hint").innerHTML = userHint;
+var computerGuess = charactersList[Math.floor(Math.random() * charactersList.length)];
+var conputerGuessName = computerGuess.name.toLowerCase();
+var conputerGuessImage = computerGuess.image;
+var conputerGuessHint = computerGuess.hint;
+// End of variable
 
-      if (question === userAnswer) {
-        console.log('The Word is ' + question);
-        console.log('The user entered '+ userAnswer);
-
-      } else{
-        console.log('The Word is ' + question);
-        console.log('The user entered '+ userAnswer);
-      }
-    } else {
-      console.log('enter not pressed');
+// Functions
+function letterChecker(letrs, playerGuess) {
+  // split word into array of individual letters, store in wordArr
+  var currentWord = letrs;
+  console.log('This is the current word: ' + currentWord);
+  var wordArr = letrs.split("");
+  console.log('The current word as an array: ' + wordArr);
+  //
+  for (var i= 0; i < wordArr.length; i++) {
+    console.log(element);
+    var element = wordArr[i];
+    if(playerGuess === element){
+      console.log('test');
     }
   }
 }
 
 
-// var word = "<p>User pressed " + userEnter + "</p>";
-// next line writes to the screen
-// document.querySelector("#gameSpace").innerHTML = word;
+//
+document.onkeyup = function (event) {
+  var userEnter = event.key.toLowerCase();
+  console.log('User pressed: ' + userEnter);
+  letterChecker(conputerGuessName, userEnter);
+}
 
-//pull whats  entered into input
-//document.querySelector('#answerF').value;
+function reset() {
+  computerGuess = charactersList[Math.floor(Math.random() * charactersList.length)];
+  conputerGuessName = computerGuess.name.toLowerCase();
+  conputerGuessImage = computerGuess.image;
+  conputerGuessHint = computerGuess.hint;
+
+}
+
+//
+// Press any key to get started!
+// Wins: (# of times user guessed the word correctly).
+
+
+
+// If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+// As the user guesses the correct letters, reveal them: m a d o _  _ a.
+
+
+
+// Number of Guesses Remaining: (# of guesses remaining for the user).
+// Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+// After the user wins/loses the game should automatically choose another word and make the user play it.
+
+
+
+// Word Guess Game Bonuses
+
+
+// Play a sound or song when the user guesses their word correctly, like in our demo.
+// Write some stylish CSS rules to make a design that fits your game's theme.
+
+// HARD MODE: Organize your game code as an object, except for the key events to get the letter guessed. This will be a challenge if you haven't coded with JavaScript before, but we encourage anyone already familiar with the language to try this out.
+// Save your whole game and its properties in an object.
+// Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
+// Don't forget to place your global variables and functions above your object.
+
+
+// Remember: global variables, then objects, then calls.
---
+++ assets/javascript/game2.js
@@ -0,0 +1,23 @@
+console.log('connected');
+//
+var charactersList = [{
+    "name": 'Buzz',
+    "image": '../images/Buzz.jpg',
+    "hint": 'To Infinity and Beyond'
+  },
+  {
+    "name": 'Woody',
+    "image": '../images/woody.png',
+    "hint": 'Reach for the sky!'
+  },
+  {
+    "name": 'Rex',
+    "image": '../images/Rex.png',
+    "hint": 'Any dinosaur-shaped ones?'
+  },
+  {
+    "name": 'Jessie',
+    "image": '../images/Jessie.png',
+    "hint": 'Buzz Lightyear\'s girlfriend'
+  },
+]
