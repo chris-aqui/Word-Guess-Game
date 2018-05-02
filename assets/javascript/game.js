2503b82c1c04eb47b6d4869580b454cf88b67589
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-02 18:32:41 -0400

    update from live-code session with demi, work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,6 +25,8 @@ var computerGuess = charactersList[Math.floor(Math.random() * charactersList.len
 var conputerGuessName = computerGuess.name.toLowerCase();
 var conputerGuessImage = computerGuess.image;
 var conputerGuessHint = computerGuess.hint;
+var lettersLeft = computerGuess.name.length;
+var isWin = false;
 var livesLeft = 10;
 var score = 0;
 var lose = 0;
@@ -37,50 +39,80 @@ var blankSpaces = [];
 function letterChecker(answer, playerGuess) {
   // split my word into an array of letters
   var wordArr = answer.split("");
-  console.log('The current word as an array: ' + wordArr);
   // push the user guess into the guesses made array
-  guessesMade.push(playerGuess);
 
+  if (playerGuess === 'x' && isWin) {
+    reset();
+    isWin = false;
+  }
   // loop over the word for each letter of the word array
   for (var i = 0; i < wordArr.length; i++) {
-    // sets the index of the word array to an element for easy readng
-    var element = wordArr[i];
-    // checks the user guess for the word array index
-    var letterIndex = wordArr.indexOf(playerGuess);
-    console.log('player guess index: ' + letterIndex);
-    // checks to see if the user value is present in the word array on each index
-    if (playerGuess === element) {
-      console.log('we are checking ' + element);
-      blankSpaces[i] = element;
-      $("#userGues").text(blankSpaces.join("\xa0"));
-      console.log("show blankspace " + blankSpaces);
-      // score++;
-      console.log('You got one, keep going');
-      // break
-    } else if(playerGuess != element) {
-      console.log('Wrong, try again');
-      livesLeft--;
-      // break
-      if (livesLeft === 0) {
-        livesLeft = 0;
-        element = 0;
-        // lose++;
-        alert("you lose!");
-        reset();
+    if (playerGuess === wordArr[i]) {
+      guessesMade.push(playerGuess);
+      blankSpaces[i] = playerGuess;
+      $("#userGues").text(blankSpaces.join(" "));
+      lettersLeft--;
+      console.log(lettersLeft);
+      console.log(wordArr + ' ----- ' + blankSpaces)
+      if (lettersLeft === 0) {
+        console.log('you win')
+        score++;
+        var youWinDiv = document.createElement("div")
+        youWinDiv.innerHTML = `You Win! Your score is ${score}. Press X to start again.`;
+        $('#userGues').append(youWinDiv);
+        return isWin = true;
+      } else {
+        return null
       }
+    } else if (playerGuess !== wordArr[i]) {
+      var filteredGuesses = guessesMade.filter( guess => guess === playerGuess)
+      if (filteredGuesses.length === 0) {
+        console.log('life lost')
+        guessesMade.push(playerGuess);
+        livesLeft--;
+      }
+
     }
+
+    // // sets the index of the word array to an element for easy readng
+    // var element = wordArr[i];
+    // // checks the user guess for the word array index
+    // var letterIndex = wordArr.indexOf(playerGuess);
+    // console.log('player guess index: ' + letterIndex);
+    // // checks to see if the user value is present in the word array on each index
+    // if (playerGuess === element) {
+    //   console.log('we are checking ' + element);
+    //   blankSpaces[i] = element;
+    //   $("#userGues").text(blankSpaces.join("\xa0"));
+    //   console.log("show blankspace with user entered value " + blankSpaces);
+    //   // score++;
+    //   console.log('You got one, keep going');
+    //   // break
+    // } else if(playerGuess != element) {
+    //   // guessesMade.push(playerGuess);
+    //   console.log('Wrong, try again');
+    //   // livesLeft--;
+    //   // break
+    //   if (livesLeft <= 0) {
+    //     livesLeft = 0;
+    //     element = 0;
+    //     // lose++;
+    //     alert("you lose!");
+    //     reset();
+    //   }
+    // }
   }
-      if(blankSpaces.length === wordArr.length ){ // for some reason this is alway true...
-            score++;
-            console.log(score);
-            // alert("you lose!");
-            console.log("this is the answer: " + blankSpaces);
-  }
+      // if(blankSpaces === wordArr){ // for some reason this is alway true...
+      //       score++;
+      //       console.log(score);
+      //       alert("You Won!!");
+      //       console.log("this is the answer: " + blankSpaces);
+  // }
 }
 //
 document.onkeyup = function (event) {
   var userEnter = event.key.toLowerCase();
-  console.log('User pressed: ' + userEnter);
+  // console.log('User pressed: ' + userEnter);
   letterChecker(conputerGuessName, userEnter);
   updateMe();
 }
@@ -95,8 +127,8 @@ function blank(x) {
       blankSpaces.push("_");
     }
   }
-  var testt = blankSpaces.join("\xa0");
-  $("#userGues").text(testt);
+  // var testt = blankSpaces.join("\xa0");
+  // $("#userGues").text(testt);
   // \xa0 is a Unicode no-break space
 }
 //
@@ -108,16 +140,17 @@ function reset() {
   livesLeft = 10;
   guessesMade = [];
   blankSpaces = [];
-  updateMe();
+  blank(conputerGuessName);
   // blank(conputerGuessName);
 }
 //
 $(document).ready(function () {
-  blank(conputerGuessName);
+  blank(computerGuess.name);
   updateMe();
 });
 
 function updateMe() {
+  $('#userGues').text(blankSpaces.join(" "))
   $("#hint").text(conputerGuessHint);
   $("#score").text(score);
   $("#guess").text(guessesMade);
